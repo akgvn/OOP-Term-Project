2c187321985f61be944fe273b4c6bd0a82687e9a
akgvn <akgvn@yandex.com>
2022-05-07 20:24:30 +0300

    Some warnings enabled

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -std=c++17 -g -o game.exe src/main.cpp src/Player.cpp src/GameObject.cpp src/Laser.cpp src/Asteroid.cpp src/GoManager.cpp src/Enemy.cpp
+g++ -std=c++17 -ansi -pedantic -Wall -Wextra -Weffc++ -g -o game.exe src/main.cpp src/Player.cpp src/GameObject.cpp src/Laser.cpp src/Asteroid.cpp src/GoManager.cpp src/Enemy.cpp
